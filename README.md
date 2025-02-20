e34d4610fbfe311431414c9c422860371a3196a5
jkoch22 <jkoch@yumaunion.org>
2025-02-20 10:54:28 -0700

    v 0.0.3 commit

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # n8n-nodes-xml-plus
 
-An alternative XML node based on fast-xml-parser as opposed to xml2js that's used in the built-in XML node. Designed for performance with larger datasets and customizability. I built this after running it some issues with the XML node that's bundled with n8n, specifically when I was working with Google's older data APIs. Chunks of this code are based of the original XML node. It might be a little buggy but hopefully someone else finds it helpful as well.
+An alternative XML node based on fast-xml-parser as opposed to xml2js that's used in the built-in XML node. Designed for performance with larger datasets as well as extra customizability. I built this after running into some issues with the built-in XML node, specifically when I was working with Google's older Data APIs. Chunks of this code are based on the original XML node. It might be a little buggy but hopefully someone else finds it useful as well.
 
 This is an n8n community node. It lets you transform XML data with a high level of customizability in your n8n workflows.
 
@@ -19,23 +19,27 @@ Follow the [installation guide](https://docs.n8n.io/integrations/community-nodes
 
 ## Operations
 
-JSON to XML
+Converting data between JSON and XML formats with the ability to format and validate XML.
 
-XML to JSON
+Additional documentation on the options included in this node can be found on the fast-xml-parser GitHub [here](https://github.com/NaturalIntelligence/fast-xml-parser/tree/master/docs/v4)
 
 ## Compatibility
 
-n8n >=v1.78.0
+Tested on v1.78.0 but will probably work with other versions as well.
 
 ## Usage
 
-Feed XML or JSON data to have it converted.
+Map XML or JSON data to have it converted.
 
 ## Resources
 
 * [n8n community nodes documentation](https://docs.n8n.io/integrations/community-nodes/)
 
 ## Version history
+
+0.0.3 - Bug fix
+
+0.0.2 - Added option to choose incoming JSON field.
 
 0.0.1 - Initial release.
 
--- nodes/XmlPlus/XmlPlus.node.ts
+++ nodes/XmlPlus/XmlPlus.node.ts
@@ -20,174 +20,184 @@ import {
 
 export class XmlPlus implements INodeType {
 	description: INodeTypeDescription = {
-			displayName: 'XML Plus',
-			name: 'XmlPlus',
-			icon: 'file:xml.svg',
-			group: [],
-			version: 1,
-			description: 'Convert data to and from XML (community node)',
-			defaults: {
-					name: 'XML Plus',
+		displayName: 'XML Plus',
+		name: 'xmlPlus',
+		icon: 'file:XmlPlus.svg',
+		group: [],
+		version: 1,
+		description: 'Convert data to and from XML (community node)',
+		defaults: {
+			name: 'XML Plus'
 		},
-			inputs: [NodeConnectionType.Main],
-			outputs: [NodeConnectionType.Main],
-			properties: [{
-							displayName: 'Conversion Mode',
-							name: 'mode',
-							type: 'options',
-							options: [{
-											name: 'JSON to XML',
-											value: 'jsonToXml',
-											description: 'Converts data from JSON to XML',
-									},
-									{
-											name: 'XML to JSON',
-											value: 'xmlToJson',
-											description: 'Converts data from XML to JSON',
-									},
-							],
-							default: 'xmlToJson',
-							description: 'The conversion needed for the input data',
+		inputs: ['main'],
+		outputs: ['main'],
+		properties: [{
+				displayName: 'Conversion Mode',
+				name: 'mode',
+				type: 'options',
+				options: [{
+						name: 'JSON to XML',
+						value: 'jsonToXml',
+						description: 'Converts data from JSON to XML',
 					},
 					{
-							displayName: 'Property Name',
-							name: 'dataPropertyName',
-							type: 'string',
-							default: 'data',
-							required: true,
-							description: 'Name of the property to that needs to be converted',
+						name: 'XML to JSON',
+						value: 'xmlToJson',
+						description: 'Converts data from XML to JSON',
 					},
-					{
-							displayName: 'Format',
-							name: 'format',
-							type: 'boolean',
-							default: true,
-							description: 'Whether parsed XML is output as a single line XML string or formatted',
-							displayOptions: {
-									show: {
-											mode: ['jsonToXml'],
-									}
-							}
-					},
-					{
-							displayName: "If your XML is inside a binary file, use the 'Extract from File' node to convert it to text first",
-							name: 'xmlNotice',
-							type: 'notice',
-							default: '',
-							displayOptions: {
-									show: {
-											mode: ['xmlToJson'],
-									}
-							},
-					},
-					{
-							displayName: 'Validator',
-							name: 'validateXml',
-							type: 'boolean',
-							default: false,
-							description: 'Whether to enable or disable the XML validator on the parsed or built XML data',
-					},
-					...xmlPlusOptions
-			]
+				],
+				default: 'xmlToJson',
+				description: 'The conversion needed for the input data',
+			},
+			{
+				displayName: 'Property Name',
+				name: 'dataPropertyName',
+				type: 'string',
+				default: 'data',
+				required: true,
+				description: 'Name of the property to that needs to be converted',
+			},
+			{
+				displayName: 'Format',
+				name: 'format',
+				type: 'boolean',
+				default: true,
+				description: 'Whether parsed XML is output as a single line XML string or formatted',
+				displayOptions: {
+					show: {
+						mode: ['jsonToXml'],
+					}
+				}
+			},
+			{
+				displayName: 'Use Entire JSON',
+				name: 'useFullJson',
+				type: 'boolean',
+				default: false,
+				description: "Whether to use the full incoming JSON or the 'Property Name'",
+				displayOptions: {
+					show: {
+						mode: ['jsonToXml'],
+					}
+				}
+			},
+			{
+				displayName: "If your XML is inside a binary file, use the 'Extract from File' node to convert it to text first",
+				name: 'xmlNotice',
+				type: 'notice',
+				default: '',
+				displayOptions: {
+					show: {
+						mode: ['xmlToJson'],
+					}
+				},
+			},
+			{
+				displayName: 'Validator',
+				name: 'validateXml',
+				type: 'boolean',
+				default: false,
+				description: 'Whether to enable or disable the XML validator on the parsed or built XML data',
+			},
+			...xmlPlusOptions
+		]
 	};
 
 	async execute(this: IExecuteFunctions): Promise < INodeExecutionData[][] > {
-			const items = this.getInputData();
-			const mode = this.getNodeParameter('mode', 0) as string;
-			const options = this.getNodeParameter('options', 0, {});
-			const dataPropertyName = this.getNodeParameter('dataPropertyName', 0);
-			const returnData: INodeExecutionData[] = [];
-			for (let itemIndex = 0; itemIndex < items.length; itemIndex++) {
-					try {
-							const item = items[itemIndex];
+		const items = this.getInputData();
+		const mode = this.getNodeParameter('mode', 0) as string;
+		const options = this.getNodeParameter('options', 0, {});
+		const dataPropertyName = this.getNodeParameter('dataPropertyName', 0);
+		const returnData: INodeExecutionData[] = [];
 
-							const validateXml = this.getNodeParameter('validateXml', itemIndex) as boolean;
-							const validatorOptions = {
-									allowBooleanAttributes: true
-							}
+		for (let itemIndex = 0; itemIndex < items.length; itemIndex++) {
+			try {
+				const item = items[itemIndex];
+				const validateXml = this.getNodeParameter('validateXml', itemIndex) as boolean;
+				const validatorOptions = {
+					allowBooleanAttributes: true
+				};
 
-							if (mode === 'xmlToJson') {
-
-									if (item.json[dataPropertyName] === undefined) {
-											throw new NodeOperationError(
-													this.getNode(),
-													`Item has no JSON property called "${dataPropertyName}"`, {
-															itemIndex
-													},
-											);
-									}
-									const xmlString = item.json[dataPropertyName] as string;
-
-									// Conditionally validate incoming XML with options
-									if (validateXml) {
-											const validation = XMLValidator.validate(xmlString, validatorOptions);
-											if (validation !== true) {
-													throw new NodeOperationError(this.getNode(), `Invalid XML: ${validation.err.msg}`, {
-															itemIndex
-													});
-											}
-									}
-
-									// Parse the XML
-									const parser = new XMLParser({
-											ignoreAttributes: false,
-											...options
-									});
-									const json = parser.parse(xmlString);
-									returnData.push({
-											json: deepCopy(json)
-									});
-
-							} else if (mode === 'jsonToXml') {
-									const format = this.getNodeParameter('format', itemIndex) as boolean;
-									const builder = new XMLBuilder({
-											ignoreAttributes: false,
-											format,
-											...options
-									});
-
-									// Build XML from JSON
-									const xml = builder.build(items[itemIndex].json);
-
-									// Conditionally validate the built XML with options
-									if (validateXml) {
-											const validation = XMLValidator.validate(xml, validatorOptions);
-											if (validation !== true) {
-													throw new NodeOperationError(this.getNode(), `Built XML is invalid: ${validation.err.msg}`, {
-															itemIndex
-													});
-											}
-									}
-
-									returnData.push({
-											json: {
-													[dataPropertyName]: xml
-											},
-											pairedItem: {
-													item: itemIndex
-											}
-									});
-							} else {
-									throw new NodeOperationError(this.getNode(), `The operation "${mode}" is not supported.`, {
-											itemIndex
-									});
-							}
-					} catch (error) {
-							if (this.continueOnFail()) {
-									returnData.push({
-											json: {
-													error: (error as Error).message
-											},
-											pairedItem: {
-													item: itemIndex
-											}
-									});
-									continue;
-							}
-							throw error;
+				if (mode === 'xmlToJson') {
+					if (item.json[dataPropertyName] === undefined) {
+						throw new NodeOperationError(this.getNode(), `Item has no JSON property called "${dataPropertyName}"`, {
+							itemIndex
+						});
 					}
-			}
 
-			return [returnData];
+					const xmlString = item.json[dataPropertyName] as string;
+
+					if (validateXml) {
+						const validation = XMLValidator.validate(xmlString, validatorOptions);
+						if (validation !== true) {
+							throw new NodeOperationError(this.getNode(), `Invalid XML: ${validation.err.msg}`, {
+								itemIndex
+							});
+						}
+					}
+
+					const parser = new XMLParser({
+						ignoreAttributes: false,
+						...options
+					});
+					const json = parser.parse(xmlString);
+					returnData.push({
+						json: deepCopy(json)
+					});
+
+				} else if (mode === 'jsonToXml') {
+					const format = this.getNodeParameter('format', itemIndex) as boolean;
+					const useFullJson = this.getNodeParameter('useFullJson', itemIndex) as boolean;
+					const builder = new XMLBuilder({
+						ignoreAttributes: false,
+						suppressBooleanAttributes: false,
+						format,
+						...options
+					});
+
+					let jsonToConvert;
+					if (useFullJson) {
+						jsonToConvert = item.json;
+					} else {
+						if (item.json[dataPropertyName] === undefined) {
+							throw new NodeOperationError(this.getNode(), `Item has no JSON property called "${dataPropertyName}"`, {
+								itemIndex
+							});
+						}
+						jsonToConvert = item.json[dataPropertyName];
+					}
+
+					const xml = builder.build(jsonToConvert);
+
+					if (validateXml) {
+						const validation = XMLValidator.validate(xml, validatorOptions);
+						if (validation !== true) {
+							throw new NodeOperationError(this.getNode(), `Built XML is invalid: ${validation.err.msg}`, {
+								itemIndex
+							});
+						}
+					}
+
+					returnData.push({
+						json: {
+							[dataPropertyName]: xml
+						},
+						pairedItem: {
+							item: itemIndex
+						},
+					});
+				}
+
+			} catch (error) {
+				if (error instanceof NodeOperationError) {
+					throw error;
+				}
+				throw new NodeOperationError(this.getNode(), `Unknown error occurred: ${error.message}`, {
+					itemIndex
+				});
+			}
+		}
+
+		return [returnData]; // Important to return processed data
 	}
 }
